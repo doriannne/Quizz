469e89b6570f93e2bb4efc80c48fddc88107fc3f
doriannne <parkhomenkogalyna@gmail.com>
2023-12-12 13:32:30 +0100

    Add binding

--- app/src/main/java/com/app/quizz/home/FragmentHome.kt
+++ app/src/main/java/com/app/quizz/home/FragmentHome.kt
@@ -6,14 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.app.quizz.R
+import com.app.quizz.databinding.FragmentHomeBinding
 
 class FragmentHome : Fragment() {
+    private val binding by lazy { FragmentHomeBinding.inflate(layoutInflater) }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
